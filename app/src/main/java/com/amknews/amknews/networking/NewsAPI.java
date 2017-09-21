f24718817f5cf8550fdb143e99019048170377c5
Alexander <mburukungu@gmail.com>
2017-09-21 11:19:37 +0300

    placed a NewsAPI key through Java API conventions

--- app/src/main/java/com/amknews/amknews/networking/NewsAPI.java
+++ app/src/main/java/com/amknews/amknews/networking/NewsAPI.java
@@ -5,4 +5,6 @@ package com.amknews.amknews.networking;
  */
 
 public class NewsAPI {
+    private static final String APIKEY = "c95773660c4042239ce4988c79606b65";
+    private static final String APIPATH = "https://newsapi.org/v1/";
 }
