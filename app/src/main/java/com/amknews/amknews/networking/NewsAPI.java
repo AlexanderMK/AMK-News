fd305d0f70b6c87ca85466fc6fbd8e545ed1633c
Alexander <mburukungu@gmail.com>
2017-09-21 11:36:51 +0300

    created a static newService object to allow many quesry requests

--- app/src/main/java/com/amknews/amknews/networking/NewsAPI.java
+++ app/src/main/java/com/amknews/amknews/networking/NewsAPI.java
@@ -1,5 +1,13 @@
 package com.amknews.amknews.networking;
 
+import com.amknews.amknews.model.GetArticlesResponse;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by alex on 9/21/17.
  */
@@ -7,6 +15,20 @@ package com.amknews.amknews.networking;
 public class NewsAPI {
     private static final String APIKEY = "c95773660c4042239ce4988c79606b65";
     private static final String APIPATH = "https://newsapi.org/v1/";
+
+    private static NewsService newsService = null;
+
+    public static NewsService getApi() {
+        if(newsService == null) {
+            // initialize NewsService
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(APIPATH)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            newsService = retrofit.create(NewsService.class);
+        }
+        return newsService;
+    }
 
     public interface NewsService {
         @GET("articles?apiKey=" + APIKEY)
