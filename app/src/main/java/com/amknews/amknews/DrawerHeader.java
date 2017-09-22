150ebad1fa3bb23e1d65608df10be4cc279596ed
Alexander <mburukungu@gmail.com>
2017-09-22 11:37:29 +0300

    created drawerheader class to call header xmls

---
+++ app/src/main/java/com/amknews/amknews/DrawerHeader.java
@@ -0,0 +1,36 @@
+package com.amknews.amknews;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+/**
+ * Created by alex on 9/22/17.
+ */
+
+
+
+    @NonReusable
+    @Layout(R.layout.drawer_header)
+    public class DrawerHeader {
+
+        @View(R.id.profileImageView)
+        private ImageView profileImage;
+
+        @View(R.id.nameTxt)
+        private TextView nameTxt;
+
+        @View(R.id.emailTxt)
+        private TextView emailTxt;
+
+        @Resolve
+        private void onResolved() {
+            nameTxt.setText("Alexander MK");
+            emailTxt.setText("mburukungu@gmail.com");
+        }
+    }
+
